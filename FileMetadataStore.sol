cc194ab459050f4e40d9ab2d45ea17b1abf504e6
cobite <chainwizard@proton.me>
2024-10-19 18:23:43 +0100

    Added timestamps
    Added timestamps

--- FileMetadataStore.sol
+++ FileMetadataStore.sol
@@ -4,13 +4,22 @@ contract FileMetadataStore {
     // Mapping from file XORname (key) to metadata XORname (value)
     mapping(string => string) private fileToMetadata;
 
+    // Mapping from file XORname to submission timestamp
+    mapping(string => uint256) private fileToTimestamp;
+
     // Function to store the XORname of a file and its metadata
     function setFileMetadata(string memory fileXORname, string memory metadataXORname) public {
         fileToMetadata[fileXORname] = metadataXORname;
+        fileToTimestamp[fileXORname] = block.timestamp;
     }
 
     // Function to retrieve the metadata XORname given the file XORname
     function getMetadata(string memory fileXORname) public view returns (string memory) {
         return fileToMetadata[fileXORname];
+    }
+
+    // Function to retrieve the timestamp of when the metadata was set
+    function getTimestamp(string memory fileXORname) public view returns (uint256) {
+        return fileToTimestamp[fileXORname];
     }
 }
