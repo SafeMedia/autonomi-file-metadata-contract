76a27e5a47277d4492c99d6ddae3c1c636814935
cobite <chainwizard@proton.me>
2024-10-19 18:43:15 +0100

    Removed xorname related items
    Removed xorname related items

--- FileHashStore.sol
+++ FileHashStore.sol
@@ -7,20 +7,21 @@ contract FileHashStore {
         uint256 timestamp; // Timestamp when the hash was stored
     }
 
-    // Mapping from XOR name to FileHashData
-    mapping(string => FileHashData) private fileHashData;
+    // Mapping from hash to FileHashData
+    mapping(bytes32 => FileHashData) private fileHashData;
 
-    // Function to store the XOR name, SHA256 hash, and timestamp
-    function setFileHashData(string memory fileXORname, bytes32 fileHash) public {
-        fileHashData[fileXORname] = FileHashData({
+    // Function to store the SHA256 hash, and timestamp
+    function setFileHashData(bytes32 fileHash) public {
+        fileHashData[fileHash] = FileHashData({
             hash: fileHash,
             timestamp: block.timestamp
         });
     }
 
     // Function to retrieve the hash data for a given file XOR name
-    function getFileHashData(string memory fileXORname) public view returns (bytes32, uint256) {
-        FileHashData memory hashData = fileHashData[fileXORname];
+    function getFileHashData(bytes32 memory fileHash) public view returns (bytes32, uint256) {
+        FileHashData memory hashData = fileHashData[fileHash];
         return (hashData.hash, hashData.timestamp);
     }
 }
+ 
